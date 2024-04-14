85e5551b2dfb9b0241f7b6d0634a87b96e7cc02e
Abhijeet  Kakade <116942456+abhijeetkakade1234@users.noreply.github.com>
2024-04-14 22:33:07 +0530

    Update and rename Remove_Element.java to 27_Remove_Element.java

---
+++ 27_Remove_Element.java
@@ -0,0 +1,17 @@
+/* 27. Remove Element */
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int n = nums.length;
+        int j = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i] != val)
+            {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
+}
