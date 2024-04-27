34131e6204ca05cd93e507e18707b41c8c7ad880
Abhijeet  Kakade <116942456+abhijeetkakade1234@users.noreply.github.com>
2024-04-27 19:43:37 +0530

    Create 1672_Richest_Customer_Wealth.java

---
+++ 1672_Richest_Customer_Wealth.java
@@ -0,0 +1,17 @@
+/* 1672. Richest Customer Wealth */
+
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int max_wealth = 0;
+        for (int i = 0; i < accounts.length; i++)
+        {
+            int wealth = 0;
+            for (int j = 0; j < accounts[i].length; j++)
+            {
+                wealth = wealth + accounts[i][j];
+            }
+            max_wealth = Math.max(max_wealth,wealth);
+        }
+        return max_wealth;
+    }
+}
