4daddb0bd39b75b1c614cc9dafe8403cc8f43d85
Abhijeet  Kakade <116942456+abhijeetkakade1234@users.noreply.github.com>
2024-06-16 10:51:51 +0530

    Create 330. Patching Array.java

---
+++ 330. Patching Array.java
@@ -0,0 +1,23 @@
+/* 330. Patching Array */
+
+class Solution {
+    public int minPatches(int[] nums, int n) {
+        long missing = 1;
+        int result = 0;
+        int i = 0;
+
+        while (missing <= n)
+        {
+            if (i < nums.length && nums[i] <= missing){
+                missing += nums[i];
+                i++;
+            }
+            else{
+                missing += missing;
+                result++;
+            }
+        }
+
+       return result;
+    }
+}
