525d44f9f401d5f949e359cab36e74e55cb673db
Abhijeet  Kakade <116942456+abhijeetkakade1234@users.noreply.github.com>
2024-06-26 08:59:16 +0530

    Update 189. Rotate Array.java

--- 189. Rotate Array.java
+++ 189. Rotate Array.java
@@ -4,9 +4,9 @@ class Solution {
     public void rotate(int[] nums, int k) {
         int n = nums.length;
         k %= n;
-        swap (nums, 0, n - 1);
-        swap (nums, 0, k - 1);
-        swap (nums, k, n - 1);
+        swap (nums, 0, n - 1); // reverse entire string -> 7,6,5,4,3,2,1
+        swap (nums, 0, k - 1); // reverse first k strings - > 5,6,7,4,3,2,1
+        swap (nums, k, n - 1); // reverse remainning -> 5,6,7,1,2,3,4
     }
 
     private void swap(int[] nums, int start, int end) {
