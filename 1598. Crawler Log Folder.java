c701ff7ef7ef25e657eda066d80037287b0d1bcd
Abhijeet  Kakade <116942456+abhijeetkakade1234@users.noreply.github.com>
2024-07-10 22:30:42 +0530

    Create 1598. Crawler Log Folder.java

---
+++ 1598. Crawler Log Folder.java
@@ -0,0 +1,16 @@
+/* 1598. Crawler Log Folder */
+
+class Solution {
+    public int minOperations(String[] logs) {
+        int depth = 0;
+        for (String log : logs){
+            if (log.equals("../")){
+                depth = Math.max(0, depth - 1);
+            }
+            else if (!log.equals("./")){
+                depth++;
+            }
+        }
+        return depth;
+    }
+}
