7c93e7d1086df581c1f0faef35098d5a621643da
Abhijeet  Kakade <116942456+abhijeetkakade1234@users.noreply.github.com>
2024-08-16 22:21:36 +0530

    Create 624. Maximum Distance in Arrays.java

---
+++ 624. Maximum Distance in Arrays.java
@@ -0,0 +1,27 @@
+/* 624. Maximum Distance in Arrays */
+
+class Solution {
+    public int maxDistance(List<List<Integer>> arrays) {
+        int smallest = arrays.get(0).get(0);
+        int largest = arrays.get(0).get(arrays.get(0).size() - 1);
+        int maxDistance = 0;
+
+        for (int i = 1; i < arrays.size(); i++){
+            maxDistance = Math.max(
+                maxDistance, Math.abs(
+                    arrays.get(i).get(arrays.get(i).size() - 1) - smallest
+                )
+            );
+
+            maxDistance = Math.max(
+                maxDistance, Math.abs(
+                    largest - arrays.get(i).get(0)
+                )
+            ); 
+
+            smallest = Math.min(smallest, arrays.get(i).get(0));
+            largest = Math.max(largest, arrays.get(i).get(arrays.get(i).size() - 1));  
+        }
+        return maxDistance;
+    }
+}
